c6b954efdf78450b815e7974d980440fd0943f75
Marigold <mojmir.vinkler@gmail.com>
2022-03-10 15:11:37 +0100

    refactor(s3): align functions with owid-catalog-py

---
+++ tests/test_s3.py
@@ -0,0 +1,28 @@
+# type: ignore
+from unittest import mock
+from owid.datautils import s3
+
+
+def test_s3_path_to_bucket_key():
+    url = "https://walden.nyc3.digitaloceanspaces.com/a/test.csv"
+    assert s3.s3_path_to_bucket_key(url) == ("walden", "a/test.csv")
+
+    url = "s3://walden/a/test.csv"
+    assert s3.s3_path_to_bucket_key(url) == ("walden", "a/test.csv")
+
+    url = "https://walden.s3.us-west-2.amazonaws.com/a/test.csv"
+    assert s3.s3_path_to_bucket_key(url) == ("walden", "a/test.csv")
+
+
+@mock.patch.object(s3.S3, "connect")
+def test_download_from_s3(connect_mock):
+    S3 = s3.S3()
+    S3.download_from_s3(
+        "https://test_bucket.nyc3.digitaloceanspaces.com/test_bucket/test.csv",
+        "test.csv",
+    )
+    assert connect_mock.return_value.download_file.call_args_list[0].args == (
+        "test_bucket",
+        "test_bucket/test.csv",
+        "test.csv",
+    )

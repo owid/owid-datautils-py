2c2b279ee3c3d2eb80c894d0f91e94c814ab7ed0
lucasrodes <lucasrodes@users.noreply.github.com>
2022-06-02 07:30:13 +0200

    Bump version: 1.2.1 → 1.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, Our World In Data'
 author = 'Our World In Data'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.1'
+release = '1.2.2'
 
 
 # -- General configuration ---------------------------------------------------

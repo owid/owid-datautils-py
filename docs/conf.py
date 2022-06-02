fdc83ff0385cfb1ffecdbd56a980b454a087c640
lucasrodes <lucasrodes@users.noreply.github.com>
2022-06-02 07:29:03 +0200

    Bump version: 1.0.0 → 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, Our World In Data'
 author = 'Our World In Data'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
--- owid/datautils/__init__.py
+++ owid/datautils/__init__.py
@@ -1,4 +1,4 @@
 """Library to support the work of the Data Team at Our World in Data."""
 
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"

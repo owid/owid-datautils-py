4a25a8a18d304c97a3212eee1e12425d5e39c8b8
lucasrodes <lucasrodes@users.noreply.github.com>
2022-06-02 05:57:25 +0200

    docs: add log

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,10 @@ release = '0.4.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +51,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'furo'
+
+html_theme_options = {
+    "sidebar_hide_name": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []#['_static']
+html_static_path = ['_static']
+
+
+html_logo = "_static/owid.png"

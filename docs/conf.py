7cd917665aca7a95126091a3c6af2d97c25679e4
Pablo Rosado <pabloarosado@gmail.com>
2022-08-15 09:13:48 +0200

    chore: Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Our World In Data"
 author = "Our World In Data"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.5"
+release = "0.4.6"
 
 
 # -- General configuration ---------------------------------------------------

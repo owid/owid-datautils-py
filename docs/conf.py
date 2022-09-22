a9dc1111a7e9ca6a84ea69fae60cf98285dc5403
Marigold <mojmir.vinkler@gmail.com>
2022-09-22 19:23:30 +0200

    chore: Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Our World In Data"
 author = "Our World In Data"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.10"
+release = "0.4.11"
 
 
 # -- General configuration ---------------------------------------------------

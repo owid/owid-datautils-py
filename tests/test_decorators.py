927705d08c209f5dc8ff57e97a869384e0ae4e08
lucasrodes <lucasrodes@users.noreply.github.com>
2022-10-17 13:10:41 +0200

    test(decorators): mock s3

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,6 +3,7 @@ from pytest import raises
 
 from unittest import mock
 from typing import Any
+import boto3
 from .mocks import MockResponse
 
 
@@ -60,7 +61,11 @@ class TestEnableDownload:
         "owid.datautils.s3.check_for_aws_profile",
         return_value=None,
     )
-    def test_download_s3_args(self, mock_download, mock_check_s3):
+    @mock.patch.object(boto3.Session, "__init__", return_value=None)
+    @mock.patch.object(boto3.Session, "client", return_value="client")
+    def test_download_s3_args(
+        self, mock_download, mock_check_s3, mock_boto_1, mock_boto_2
+    ):
         func = enable_file_download(path_arg_name="path")(_test_local_file)
         func("s3://example.com/file.json")
 

92fc2652b466016fd0fa68ebacb7d7e2a1a14271
lucasrodes <lucasrodes@users.noreply.github.com>
2022-10-20 14:18:37 +0200

    feat(df): read from multiple files

--- owid/datautils/io/df.py
+++ owid/datautils/io/df.py
@@ -1,50 +1,84 @@
 """DataFrame io operations."""
 import pandas as pd
+from pathlib import Path
 from owid.datautils.decorators import enable_file_download
-from typing import Any
+from typing import Any, Optional, Union, List
 
 
-@enable_file_download("filepath")
-def read_csv(filepath: str, **kwargs: Any) -> pd.DataFrame:
-    """Read a CSV.
+COMPRESSION_SUPPORTED = ["gz", "bz2", "zip", "xz", "zst", "tar"]
 
-    It uses standard pandas function pandas.read_csv but adds the ability to read from a URL in some cases where
-    pandas.read_csv does not work.
+
+@enable_file_download("file_path")
+def from_file(
+    file_path: Union[str, Path], file_type: Optional[str] = None, **kwargs: Any
+) -> Union[pd.DataFrame, List[pd.DataFrame]]:
+    """Read a CSV or excel file.
+
+    It uses standard pandas function pandas.read_* but adds the ability to read from a URL in some
+    cases where pandas does not work.
+
+    The function will infer the extension of `file_path` by simply taking what follows the last ".". For example:
+    "file.csv" will have an extension ".csv"; "http://my/domain/file.xlsx" will have an extension "xlsx".
+
+    Reading from compressed files will not work by default, unless you provide a `file_type`.
 
     Parameters
     ----------
     filepath : str
         Path or url to file.
+    file_type : str
+        File type. For instance, if reading from a zip-compressed file that originally was a CSV,
+         `file_type` should be "csv". Reading from compressed files is supported for "csv", "dta" and "json".
     kwargs :
-        pandas.read_csv arguments.
+        pandas.read_* arguments.
 
     Returns
     -------
     pandas.DataFrame:
         Read dataframe.
     """
-    df: pd.DataFrame = pd.read_csv(filepath, **kwargs)
-    return df
+    # Ensure file_path is a Path object.
+    file_path = Path(file_path)
 
+    # Ensure extension is lower case and does not start with '.'.
+    extension = file_path.suffix.lstrip(".").lower()
 
-@enable_file_download("filepath")
-def read_excel(filepath: str, **kwargs: Any) -> pd.DataFrame:
-    """Read an excel file (xlsx or xls).
+    # If compressed file, raise an exception unless file_type is given
+    if extension in COMPRESSION_SUPPORTED:
+        if file_type:
+            extension = file_type
+        else:
+            raise ValueError(
+                "To be able to read from a compressed file, you need to provide a value"
+                " for `file_type`."
+            )
 
-    It uses standard pandas function pandas.read_excel but adds the ability to read from a URL in some cases where
-    pandas.read_excel does not work.
+    # Check path is valid
+    if not file_path.exists():
+        raise FileNotFoundError(f"Cannot find file: {file_path}")
 
-    Parameters
-    ----------
-    filepath : str
-        Path or url to file.
-    kwargs :
-        pandas.read_excel arguments.
+    # Available output methods (some of them may need additional dependencies to work).
+    output_methods = {
+        "csv": pd.read_csv,
+        "dta": pd.read_stata,
+        "feather": pd.read_feather,
+        "hdf": pd.read_hdf,
+        "html": pd.read_html,
+        "json": pd.read_json,
+        "parquet": pd.read_parquet,
+        "pickle": pd.read_pickle,
+        "pkl": pd.read_pickle,
+        "xlsx": pd.read_excel,
+        "xml": pd.read_xml,
+    }
+    if extension not in output_methods:
+        raise ValueError(
+            "Failed reading dataframe because of an unknown file extension:"
+            f" {extension}"
+        )
+    # Select the appropriate storing method.
+    read_function = output_methods[extension]
 
-    Returns
-    -------
-    pandas.DataFrame:
-        Read dataframe.
-    """
-    df: pd.DataFrame = pd.read_excel(filepath, **kwargs)
+    # Load file using the chosen read function and the appropriate arguments.
+    df: pd.DataFrame = read_function(file_path, **kwargs)
     return df
--- tests/io/test_df.py
+++ tests/io/test_df.py
@@ -1,32 +1,95 @@
 """Test functions in owid.datautils.io.local module.
 
 """
-from owid.datautils.io.df import read_csv, read_excel
+from owid.datautils.io.df import from_file
 from typing import Any
 import pandas as pd
+from pytest import raises
 
 
 class TestLoadDf:
-    df_original = pd.DataFrame([[1, 2, 3]], columns=["a", "b", "c"])
+    df_original = pd.DataFrame([[1, 2, 3], [4, 5, 6]], columns=["a", "b", "c"])
 
-    def test_read_csv(self, tmpdir):
-        file = tmpdir / "test.csv"
-        _create_csv_file(file)
-        df = read_csv(str(file))
+    def test_from_file_basic(self, tmpdir):
+        output_methods = {
+            "csv": self.df_original.to_csv,
+            # "dta": self.df_original.to_stata,
+            "feather": self.df_original.to_feather,
+            "parquet": self.df_original.to_parquet,
+            "pickle": self.df_original.to_pickle,
+            "pkl": self.df_original.to_pickle,
+            "xlsx": self.df_original.to_excel,
+            "xml": self.df_original.to_xml,
+        }
+        for extension, funct in output_methods.items():
+            file = tmpdir / f"test.{extension}"
+            if extension in ["dta", "pickle", "pkl", "feather"]:
+                funct(file)
+            else:
+                funct(file, index=False)
+            df = from_file(str(file))
+            assert df.equals(self.df_original)
+
+    def test_from_file_json(self, tmpdir):
+        file = tmpdir / "test.json"
+        self.df_original.to_json(file, orient="records")
+        df = from_file(str(file))
+        assert df.equals(self.df_original)
+        # Compressed
+        file = tmpdir / "test.zip"
+        self.df_original.to_json(file, orient="records")
+        df = from_file(str(file), file_type="json")
         assert df.equals(self.df_original)
 
-    def test_read_excel(self, tmpdir):
-        file = tmpdir / "test.xlsx"
-        _create_excel_file(file)
-        df = read_excel(str(file))
+    def test_from_file_html(self, tmpdir):
+        file = tmpdir / "test.html"
+        self.df_original.to_html(file, index=False)
+        df = from_file(str(file))[0]
         assert df.equals(self.df_original)
+
+    def test_from_file_hdf(self, tmpdir):  # hdf5?
+        file = tmpdir / "test.hdf"
+        self.df_original.to_hdf(file, key="df")
+        df = from_file(str(file))
+        assert df.equals(self.df_original)
+
+    def test_from_file_dta(self, tmpdir):
+        file = tmpdir / "test.dta"
+        self.df_original.to_stata(file, write_index=False)
+        df = from_file(str(file))
+        assert df.astype(int).equals(self.df_original.astype(int))
+        # Compressed
+        file = tmpdir / "test.zip"
+        self.df_original.to_stata(file, write_index=False)
+        df = from_file(str(file), file_type="dta")
+        assert df.astype(int).equals(self.df_original.astype(int))
+
+    def test_from_file_csv_zip(self, tmpdir):
+        file = tmpdir / "test.zip"
+        self.df_original.to_csv(file, index=False)
+        df = from_file(str(file), file_type="csv")
+        assert df.equals(self.df_original)
+
+    def test_from_file_filenotfound(self, tmpdir):
+        """File does not exist."""
+        with raises(FileNotFoundError):
+            file = tmpdir / "test.csv"
+            _ = from_file(str(file))
+
+    def test_from_file_zip_err(self, tmpdir):
+        """Compressed file, but no file type is given."""
+        with raises(ValueError):
+            file = tmpdir / "test.zip"
+            _ = from_file(str(file))
+
+    def test_from_file_format_err(self, tmpdir):
+        """Unknown file format."""
+        with raises(ValueError):
+            file = tmpdir / "test.error"
+            _create_csv_file(file)
+            _ = from_file(str(file))
 
 
 def _create_csv_file(file: Any) -> Any:
-    content = "a,b,c\n1,2,3"
+    content = "a,b,c\n1,2,3\n4,5,6"
     file.write_text(content, encoding="utf-8")
-
-
-def _create_excel_file(file: Any) -> Any:
-    df = pd.DataFrame([[1, 2, 3]], columns=["a", "b", "c"])
-    df.to_excel(file, index=False)
